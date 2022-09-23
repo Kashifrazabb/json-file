cf8b9536cf894566413e4afb437e5e62271d19bd
Kashif Raza <mymahi.0001@gmail.com>
2022-09-24 00:58:32 +0500

    hostname

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
+import os from "os"
 import data from "./db.json" assert {type: "json"}
 
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send(JSON.stringify(data))
+    res.send(os.hostname())
 })
 
 export default router
