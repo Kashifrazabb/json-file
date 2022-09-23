587aa609444dc3c7f3854a61551f55ee38bde3b3
Kashif Raza <mymahi.0001@gmail.com>
2022-09-24 01:36:51 +0500

    s

--- router.js
+++ router.js
@@ -1,13 +1,15 @@
 import { Router } from "express"
-import MobileDetect from "mobile-detect"
 import os from "os"
 import data from "./db.json" assert {type: "json"}
 
 const router = Router()
 
 router.get("/", (req, res) => {
-    var md = new MobileDetect(req.headers['user-agent']);
-    res.json(md.version('Webkit'));
+    var host = os.hostname()
+    if (host === "DESKTOP-MHCNGL6")
+        res.json("OK")
+    else
+        res.json("NOT OK")
 })
 
 export default router
