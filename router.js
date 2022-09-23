fec262d8928aec5a390fc5e3577df64c293522f5
Kashif Raza <mymahi.0001@gmail.com>
2022-09-24 01:10:13 +0500

    test

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 import { Router } from "express"
+import MobileDetect from "mobile-detect"
 import os from "os"
 import data from "./db.json" assert {type: "json"}
 
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send(os.hostname())
+    var md = new MobileDetect(req.headers['user-agent']);
+    // res.send(os.hostname())
+    res.send(md.versionStr)
 })
 
 export default router
