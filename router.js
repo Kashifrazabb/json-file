6e6654ee27e792f90b4c62af2e128e1355a1bde2
Kashif Raza <mymahi.0001@gmail.com>
2022-09-24 01:01:10 +0500

    test

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ import data from "./db.json" assert {type: "json"}
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send(os.hostname())
+    res.send("KASHIF")
 })
 
 export default router
