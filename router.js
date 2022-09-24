a661652925ebd876433e41a28495d0cf81d15bee
Kashif Raza <mymahi.0001@gmail.com>
2022-09-25 01:05:31 +0500

    ip

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import data from "./db.json" assert {type: "json"}
+import address from "address"
 
 const router = Router()
 
 router.get("/", (req, res) => {
-    const ua = req.headers["user-agent"]
-    res.send(Object.values(data.users).includes(ua))
+    // const ua = req.headers["user-agent"]
+    // res.send(Object.values(data.users).includes(ua))
+    res.send(address.ip())
 })
 
 export default router
