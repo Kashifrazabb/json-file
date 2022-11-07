b366c11089f3da9e0ab4302e337093d17b25f85d
Kashif Raza <mymahi.0001@gmail.com>
2022-11-08 00:21:02 +0500

    added platforms

--- router.js
+++ router.js
@@ -3,12 +3,19 @@ import data from "./db.json" assert { type: "json" }
 
 const router = Router()
 
-router.get("/mob", (req, res) => {
-    res.send(data.users__mob)
-})
-
-router.get("/com", (req, res) => {
-    res.send(data.users__pc)
+router.get("/", (req, res) => {
+    var site = req.query.site.replace(".com", "");
+    var sig = req.query.sig;
+    var val;
+    data[site].forEach(item => {
+        if (item.sig === sig) {
+            val = true
+        }
+    })
+    if (!val) {
+        res.send(false)
+    }
+    res.send(val)
 })
 
 export default router
