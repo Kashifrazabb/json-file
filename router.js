fa1e361679bddaa0e36a0d8be9e715a949260c82
Kashif Raza <mymahi.0001@gmail.com>
2022-11-10 00:02:22 +0500

    as

--- router.js
+++ router.js
@@ -6,14 +6,13 @@ const router = Router()
 router.get("/v1", (req, res) => {
     var site = req.query.site.replace(".com", "");
     var sig = req.query.sig;
-    // var val = false;
-    // for (let i = 0; i < data[site].length; i++) {
-    //     if (data[site][i].sig === sig) {
-    //         val = true
-    //         break
-    //     }
-    // }
-    res.send(`${sig} ${site}`)
+    var val = false;
+    for (let i = 0; i < data[site].length; i++) {
+        if (data[site][i].sig === sig) {
+            val = true
+        }
+    }
+    res.send(val)
 })
 
 export default router
