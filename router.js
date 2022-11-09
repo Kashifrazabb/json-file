1a8a941938183fc0db44304ab071d7cb20e618a7
Kashif Raza <mymahi.0001@gmail.com>
2022-11-10 00:16:23 +0500

    as

--- router.js
+++ router.js
@@ -1,11 +1,17 @@
-import { Router } from "express"
+import e, { Router } from "express"
 import data from "./db.json" assert { type: "json" }
 
 const router = Router()
 
 router.get("/v1", (req, res) => {
     var site = req.query.site.replace(".com", "");
-    var sig = req.query.sig.replace("%3B", ";");
+    var sig;
+    if (req.query.sig.includes("%3B")) {
+        sig = req.query.sig.replace("%3B", ";");
+    }
+    else {
+        sig = req.query.sig
+    }
     var val = false;
     for (let i = 0; i < data[site].length; i++) {
         if (data[site][i].sig === sig) {
